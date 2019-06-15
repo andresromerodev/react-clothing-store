53ef40fbdc2705a259abe890366d9db82ad603ec
Andrés Romero <andres.romero22@outlook.com>
2019-06-15 11:08:47 -0600

    First commit: Project Setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log('App listening on port 8080!');
+});
