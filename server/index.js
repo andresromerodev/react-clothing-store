2d363243680efa85706c483f73ec680d6751223d
Andrés Romero <andres.romero22@outlook.com>
2019-06-19 18:30:33 -0600

    New server struture for source files

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.post('/v1/users', (req, res) => {
     const username = req.body.username;
     const email = req.body.email;
     const role = req.body.role;
-    console.log('post: data => ', username, email, role);
+    console.log('post: data => ', username, email, role);    
 
     res.status(200).end();
 });
