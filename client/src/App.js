e66262e3b4e73bd86302d1df9d23745a1587b83d
Andrés Romero <andres.romero22@outlook.com>
2019-06-22 17:41:15 -0600

    Create React App

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello world!</h1>
+        <p>
+          This is my first react app.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-export const UserSchema = new Schema({
-  email: String,
-  username: String,
-  role: String,
-});
-
-export const UserModel = mongoose.model('User', UserSchema);
