273c934aed2e3577070d9fe5c2db252b7c248c31
Andrés Romero <andres.romero22@outlook.com>
2019-07-01 17:47:46 -0600

    Few Components Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,71 @@
-import React, { Component } from 'react'
-import './App.css'
+/* eslint-disable no-unused-vars */
+
+import React, { Component } from 'react';
+import './App.css';
+import products from './data/products'
+import ProductList from './components/product/ProductList';
+import Form from './components/Form';
 import TextInput from './components/inputs/TextInput';
+import PasswordInput from './components/inputs/PasswordInput';
+import CheckboxInput from './components/inputs/CheckboxInput';
+import {PrimaryButton, SecondaryButton} from './components/Button';
 
 class App extends Component {
-
   state = {
     firstName: '',
-    lastName: ''
+    lastName: '',
+    password: '',
+    sendEmail: true,
   };
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+  handleTextChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleBooleanChange = e => {
+    this.setState({ [e.target.name]: e.target.checked });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
   render() {
     return (
       <div className="App">
-
-        <TextInput 
-          value={this.state.firstName}
-          name="firstName"
-          onChange={this.handleChange}
-        />
-        <TextInput 
-          value={this.state.lastName}
-          name="lastName"
-          onChange={this.handleChange}
-        />
-
-        {/* <ProductList products={products}/> */}
+        <Form onSubmit={this.handleSubmit}>
+          <TextInput
+            label="First Name"
+            value={this.state.firstName}
+            name="firstName"
+            onChange={this.handleTextChange}
+          />
+          <TextInput
+            label="Last Name"
+            value={this.state.lastName}
+            name="lastName"
+            onChange={this.handleTextChange}
+          />
+          <PasswordInput
+            label="Password"
+            value={this.state.password}
+            name="password"
+            onChange={this.handleTextChange}
+          />
+          <CheckboxInput
+            label="Can we send you promo email?"
+            checked={this.state.sendEmail}
+            name="sendEmail"
+            onChange={this.handleBooleanChange}
+          />
+          <PrimaryButton>
+            Submit
+          </PrimaryButton>
+        </Form>
+        {/*<ProductList products={products} />*/}
       </div>
-    )
+    );
   }
 }
 
